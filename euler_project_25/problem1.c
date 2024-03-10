776a819189fd87653685335e68e74a36ebc8011c
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-10 15:51:43 -0300

    Update problem1.c

--- euler_project_25/problem1.c
+++ euler_project_25/problem1.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include <stdio.h>  /*entrada e saida*/
 
-void main () {
+int main () {
 
-  //declaracao das variaveis
-  int x;
-  int sum;
+  int x;      /*declaracao das variaveis*/
+  int sum=0;
 
-  for (x=1, x<1000; x++){
-    if (((x%3)==0) || ((x%5)==0)) {
-      sum=sum+x;
+  for (x=1, x<1000; x++){           
+    if (((x % 3) == 0) || ((x % 5) == 0)) {  /*verifica se o numero é divisivel por 3 e 5*/
+      sum+=x;                                /*valor adicionado*/
     }
   }
-  printf("%d \n, sum);
+  printf("A soma dos números divisíveis por 3 ou 5 até 999 é: %d\n", sum);
+  return 0;
+}
